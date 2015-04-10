2435a57e74b3bd7668b7047e77a00874039079f0
Juan Patulandong <jpatu001@hammer.cs.ucr.edu>
2015-04-09 17:18:46 -0700

    added main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+#include<unistd.h> //fork(), execvp(), etc
+#include<sys/wait.h> //wait calls
+#include<stdio.h> //perror
+#include<errno.h> //
+#include<dirent.h> //Directory Syscalls
+#include<sys/stat.h> //
+#include<sys/types.h> //
+#include<signal.h> //signal(),kill(), etc
+#include<pwd.h> //pwd()
+#include<sys/ioctl.h> //ioctl()
+
+using namespace std;
+
+int main()
+{
+
+
+
+
+
+	return 0;
+}
