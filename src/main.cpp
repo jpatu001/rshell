08aa98ab416cdbd95a32f8b2c80e4621e177fb12
Juan Patulandong <jpatu001@hammer.cs.ucr.edu>
2015-04-09 23:12:10 -0700

    added user@host

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,53 @@
 #include<signal.h> //signal(),kill(), etc
 #include<pwd.h> //pwd()
 #include<sys/ioctl.h> //ioctl()
-
+#include<stdlib.h>
 using namespace std;
 
 int main()
 {
+ 	string name;
+	name = getlogin();
+	if(name=="")
+	{
+		perror("getlogin()");
+	}
 
+	//Retrieves hostname
+	char host[64];
+	gethostname(host, 64);	
+	if (gethostname(host, sizeof(host-1))==-1)
+	{
+		perror("gethostname()");
+	}	
+	//Removes cs.ucr.edu
+	int i = 0;
+	bool isFixed = false;
+	while(!isFixed)
+	{
+		if(host[i]=='.'){
+			host[i] = '\0';
+			isFixed = true;
+		}
+		++i;
+	}
+	
+	name = name + '@' + host;
+	
+	string str;
+	while(1)
+	{
+		cout << name << "$ ";
+        cin >> str;
+        if(str=="exit")
+		{
+			exit(1);
+		}
+		else
+		{
+			continue;
+		}
+	}
 
 
 
