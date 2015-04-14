56d9a4f41111e2b08f167bafb2b075d866e077b0
Juan Patulandong <jpatu001@hammer.cs.ucr.edu>
2015-04-13 17:39:46 -0700

    execvp running

--- src/main.cpp
+++ src/main.cpp
@@ -11,25 +11,49 @@
 #include<pwd.h> //pwd()
 #include<sys/ioctl.h> //ioctl()
 #include<stdlib.h>
+#include<cstring>
+#include<string.h>
+#include<vector>
+#include<queue>
 using namespace std;
+
+
+//Universal Variables
+
+void parse(char *cmd)
+{
+	char **arg = (char**) malloc(1000);
+	int iter = 0;
+	arg[iter] = strtok(cmd," ");	
+	iter++;	
+	cout << "Gets here\n";
+	
+	while((arg[iter]=strtok(NULL," "))!=NULL)
+	{
+		iter++;
+	}
+	cout << "Puts NULL\n";
+	arg[iter] = NULL;
+	if(execvp(arg[0], arg)==-1) perror("execvp");
+			
+
+}
+void findOPS(string cmd);
 
 int main()
 {
+
+	queue<string> ops;
  	string name;
 	//Gets user login name
 	name = getlogin();
-	if(name=="")
-	{
-		perror("getlogin()");
-	}
+	if(name=="") perror("getlogin()");
 
 	//Retrieves hostname
 	char host[64];
 	gethostname(host, 64);	
-	if (gethostname(host, sizeof(host)-1)==-1)
-	{
-		perror("gethostname()");
-	}	
+	if (gethostname(host,sizeof(host)-1)==-1) perror("gethostname()");
+
 	//Removes cs.ucr.edu
 	int i = 0;
 	bool isFixed = false;
@@ -41,27 +65,44 @@ int main()
 		}
 		++i;
 	}
-	
 	name = name + '@' + host;
 	
-	string str;
+	string userIN;
 	while(1)
 	{
-	//	char input[1000];
-		cout << name << "$ ";
-        cin >> str;
-        if(str=="exit")
-		{
-			exit(1);
-		}
-		else
-		{
-			continue;
-		}
+	//	int h = fork();
+
+		//if(h==0){
+			cout << name << "$ ";
+        	getline(cin, userIN);
+	
+			//Checks for # and removes everything after
+			char* command = (char*)userIN.c_str();
+			if(userIN.find('#')!=string::npos) userIN = userIN.substr(0, userIN.find('#'));
+		
+			if(userIN=="exit")
+			{
+				return 0;
+			}
+			parse(command);
+		//}
+		//else if(h>0){
+		//	wait(0);
+		//}
+		//else perror("IDK");
+
 	}
 
 
 
 
 	return 0;
+}
+
+
+void findOPS(string cmd)
+{
+
+
+
 }
