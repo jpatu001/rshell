5719126bcb620189236a316d9cecf7846d0f39a8
Juan Patulandong <jpatu001@hammer.cs.ucr.edu>
2015-05-01 19:24:27 -0700

    colors working

--- src/ls.cpp
+++ src/ls.cpp
@@ -7,7 +7,7 @@
 #include<dirent.h>	//Directory Syscalls
 #include<sys/stat.h> 
 #include<sys/types.h>
-#include<signal.h>	//signal(),kill(), etc
+#include<signal.h>	//signal(),kill(), etC
 #include<pwd.h>		//pwd()
 #include<sys/ioctl.h>	//ioctl()
 #include<stdlib.h>
@@ -26,7 +26,7 @@ using namespace std;
 #define PRINT_HID cout << "\033[47;m";
 #define PRINT_DIR cout << "\033[34m";
 #define PRINT_DIRHID cout << "\033[1;47;34m";
-#define PRINT_EXE cout << "\033[1;47;32m";
+#define PRINT_EXE cout << "\033[1;32m";
 #define PRINT_EXEHID cout << "\033[1;47;32m";
 #define OUT_DEFAULT cout << "\033[0;00m";
 
@@ -77,7 +77,6 @@ void printFiles(const char* directory, bool& dashA, bool& dashL, bool& dashR,boo
 		string tmp(directory);
 		if(directory2.at(directory2.size()-1)!='/') tmp+='/';
 		tmp+= files.at(i);
-		//cout << endl << tmp << endl;
 		if( ( stat(tmp.c_str(), &s ) )==-1 )
 		{
 			perror("stat()");
@@ -114,13 +113,17 @@ void printFiles(const char* directory, bool& dashA, bool& dashL, bool& dashR,boo
 	if(dashL) cout << "total " << (totalBlocks/2) << endl;
 	for(unsigned int i = 0; i < files.size(); i++)
 	{
-		/*
-		struct stat s;
-		if( ( stat(files.at(i).c_str(), &s ) )==-1 )
+ 		struct stat s;
+ 		string directory2(directory);
+		string tmp(directory);
+		if(directory2.at(directory2.size()-1)!='/') tmp+='/';
+		tmp+= files.at(i);
+		if( ( stat(tmp.c_str(), &s ) )==-1 )
 		{
 			perror("stat()");
 			exit(1);
 		}	
+		//=== COLOR ====================================================
 		bool is_dir = false;
 		bool is_exe = false;
 		OUT_DEFAULT //RESETS OUTPUT COLOR
@@ -129,34 +132,35 @@ void printFiles(const char* directory, bool& dashA, bool& dashL, bool& dashR,boo
 			if(files.at(i).at(0)!='.') PRINT_DIR//DIRECTORY
 			else if(files.at(i).at(0)=='.') PRINT_DIRHID//HIDDEN_DIR
 		}
-		if(s.st_mode & S_IXUSR){
-			cout << "HERE";
+		if(s.st_mode & S_IXUSR && !is_dir){
 			is_exe = true;
 			if(files.at(i).at(0)!='.') PRINT_EXE 
-			else if(files.at(i).at(0)=='.') PRINT_EXEHID
+			if(files.at(i).at(0)=='.') PRINT_EXEHID
 		}
-		if(files.at(i).at(0)=='.' && !is_exe && !is_dir) PRINT_HID*/
+		if(files.at(i).at(0)=='.' && !is_exe && !is_dir) PRINT_HID
+		//=============================================================
 		if(!dashL){
 			if(currW>=maxW){
 				cout << endl;
 				currW = 0;
 			}
 			currW+= files.at(i).size()+1;
-			cout << setw(w) << left << files.at(i) << "  ";
-			OUT_DEFAULT
+			cout << setw(w) << left << files.at(i);
+			OUT_DEFAULT //Resets cout color
+			cout << "  ";
 		} 
 		else if(dashL){
 			string directory2(directory);
 			string tmp(directory);
 			if(directory2.at(directory2.size()-1)!='/') tmp+='/';
 			tmp+= files.at(i);
-
 			printPerm(files.at(i), tmp);
 		}
-		//OUT_DEFAULT
+		OUT_DEFAULT //Resets cout color
 		if(files.size()==i -1) cout << endl << endl;
 	}
 	if(!dashL) cout << endl;
+	//RECURSIVE PRINTFILES: for -R
 	if(dashR)
 	{	
 		for(unsigned int i = 0; i < dir.size(); i++)
@@ -176,12 +180,16 @@ int main(int argc, char** argv)
 	bool dashL = false;
 	bool dashR = false;
 	bool isMultDir = false;
-
+	
+	//NOT ENOUGH ARGV
 	if(argc < 1)//Not enough Arguments passed in
 	{
 		cout << "No Arguments Passed" << endl;
 		exit(1);
 	}
+	//======================================================================================
+	//Multiple Arguments with optional paths
+	//======================================================================================
 	else
 	{
 		//Takes all user input, and looks for the flags 
@@ -230,37 +238,40 @@ int main(int argc, char** argv)
 				int currWidth = 0;
 				for(unsigned int i = 0; i < files.size(); i++)
 				{
-					/*
-					bool is_exe = false;
-					bool is_dir = false;
-					struct stat s;
-					if( ( stat(files.at(i).c_str(), &s ) )==-1 )
-					{
-						perror("stat()");
-						exit(1);
-					}*/
 					if(currWidth>=maxWidth)
 					{
 						currWidth = 0;
 						cout << endl;
 					}
+					struct stat s;
+					//=== COLOR ====================================================
+					bool is_dir = false;
+					bool is_exe = false;
+					OUT_DEFAULT //RESETS OUTPUT COLOR
 					if(dashL) printPerm(files.at(i), ".");
-					else{/*
-						OUT_DEFAULT //RESETS OUTPUT COLOR
-						if(S_ISDIR(s.st_mode)){
-							is_dir = true;
-							if(files.at(i).at(0)!='.') PRINT_DIR//DIRECTORY
-							else if(files.at(i).at(0)=='.') PRINT_DIRHID//HIDDEN_DIR
-						}
-						if(S_IXUSR & s.st_mode){
-							is_exe = true;
-							if(files.at(i).at(0)!='.') PRINT_EXE 
-							else if(files.at(i).at(0)=='.') PRINT_EXEHID
-						}
-						if(files.at(i).at(0)=='.' && !is_exe && !is_dir) PRINT_HID*/
+					if( ( stat(files.at(i).c_str(), &s ) )==-1 )
+					{
+						perror("stat()");
+						exit(1);
+					}
+					if(S_ISDIR(s.st_mode)){
+						is_dir = true;
+						if(files.at(i).at(0)!='.') PRINT_DIR//DIRECTORY
+						else if(files.at(i).at(0)=='.') PRINT_DIRHID//HIDDEN_DIR
+					}
+					if(s.st_mode & S_IXUSR && !is_dir){
+						is_exe = true;
+						if(files.at(i).at(0)!='.') PRINT_EXE 
+						if(files.at(i).at(0)=='.') PRINT_EXEHID
+					}
+					if(files.at(i).at(0)=='.' && !is_exe && !is_dir) PRINT_HID
+					//=== OUTPUT =================================================
+					else{
 						currWidth += files.at(i).size() + 2;
-						cout << files.at(i) << "  ";
-						//OUT_DEFAULT //RESETS OUTPUT COLOR
+						cout << files.at(i);
+						OUT_DEFAULT
+						cout << "  ";
+
 					}
 				}
 				cout << endl;
@@ -275,7 +286,11 @@ int main(int argc, char** argv)
 				}
 			}
 		}
-		else{//Ouput files in the current directory
+		//==================================================================================
+		//NO PATH GIVEN, DEFAULT TO ..
+		//==================================================================================
+		else
+			{//Ouput files in the current directory
 			printFiles(".", dashA, dashL, dashR, isMultDir);
 		}
 	}
@@ -284,17 +299,23 @@ int main(int argc, char** argv)
 
 void printPerm(const string & file, const string& path)
 {
+	OUT_DEFAULT
 	struct group  *group;
 	struct passwd *passwd;
 	struct stat s;
+	bool isExe = false;
+	bool isDir = false;
 	if( ( stat(path.c_str(), &s ) )==-1 )
 	{
 		perror("stat()");
 		exit(1);
 	}	
-	//Acces Permissions
+	//Access Permissions
 	if(S_ISREG(s.st_mode)) cout << "-";
-	else if(S_ISDIR(s.st_mode)) cout << "d";
+	else if(S_ISDIR(s.st_mode)){
+		isDir = true;
+		cout << "d";
+	}
 	else if(S_ISCHR(s.st_mode)) cout << "c";
 	else if(S_ISBLK(s.st_mode)) cout << "b";
 	else if(S_ISLNK(s.st_mode)) cout << "l";
@@ -320,11 +341,26 @@ void printPerm(const string & file, const string& path)
 	localtime_r(&t, &lt);
 	char time[80];
 	strftime(time,sizeof(time), "%c", &lt);
-	//===OUTPUT=============================
 	cout << setw(8) << passwd->pw_name;
 	cout << setw(8) << group->gr_name;
 	cout << setw(8) << s.st_size << " ";
 	cout << setw(20) << time << " ";
-	cout << file << endl;
-	//delete[] time;
+	//===COLORS=============================
+	if(isDir)
+	{
+		if(file.at(0)!='.') PRINT_DIR
+		if(file.at(0)=='.') PRINT_DIRHID
+	}
+	if(s.st_mode & S_IXUSR && !isDir)
+	{
+		isExe = true;
+		if(file.at(0)!='.') PRINT_EXE
+		if(file.at(0)=='.') PRINT_EXEHID
+
+	}
+	if(file.at(0)=='.' && !isDir && !isExe) PRINT_HID
+	//===OUTPUT=============================
+	cout << file;
+	OUT_DEFAULT
+	cout << endl;
 }
